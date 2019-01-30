4fb215f67b4c48097d4bb6291ae63e80e7d11e59
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-01-30 18:24:46 +0200

    Add first component (Manualy)

--- 01 intro/src/app/app.module.ts
+++ 01 intro/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TestComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
