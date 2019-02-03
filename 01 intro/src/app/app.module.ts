93d289e3325283a0b59ad9347a305feaf134356e
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-02-04 00:17:51 +0200

    Created component using 'ng g c footer'

--- 01 intro/src/app/app.module.ts
+++ 01 intro/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HeadComponent } from './head/head.component';
 import { MainComponent } from './main/main.component';
 import { ListComponent } from './main//List/list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeadComponent, MainComponent, ListComponent],
+  declarations: [AppComponent, HeadComponent, MainComponent, ListComponent, FooterComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
